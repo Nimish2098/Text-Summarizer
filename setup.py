38dfab9302b4040a67b7a0e016d04d50f0f3a42c
Nimish2098 <raonimish23@gmail.com>
2025-05-26 21:49:55 +0530

    Logging code

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setuptools.setup(
     project_urls={
         "Bug Tracker":f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}"
     },
-    package_dir={"":",src"}
+    package_dir={"":"src"},
     packages=setuptools.find_packages(where="src")
-    
 )
